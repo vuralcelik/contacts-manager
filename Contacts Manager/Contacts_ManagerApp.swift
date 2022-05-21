fd182dad444a5f0e3499f2672d41f9bb1a0bc6a8
Macbook <hitget.clk@gmail.com>
2022-05-21 14:31:58 +0300

    Add initial files to project

---
+++ Contacts Manager/Contacts_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_ManagerApp.swift
+//  Contacts Manager
+//
+//  Created by Vural Çelik on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
